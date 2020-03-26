16da45d9ac48ceb5ba759f1f81594aa3177b8ff5
liaochenhan <932404058@qq.com>
2020-03-26 13:26:15 +0800

    20200326 perspective correction interlation

---
+++ RasterizationRenderer/PerspectiveCorrectionInterpolation/Canvas.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <graphics.h>
+
+class Canvas
+{
+public:
+	Canvas(int width, int height);
+	~Canvas();
+
+	void setPixel(int x, int y, COLORREF color);	// 绘制像素点
+
+	int mScrWidth, mScrHeight;						// 画布的大小
+private:
+	DWORD* pImageBuf;								// 显存指针
+};
+
+Canvas::Canvas(int width, int height)
+{
+	initgraph(width, height);						// 初始化窗口
+	pImageBuf = GetImageBuffer();					// 获取显存指针
+	mScrWidth = width, mScrHeight = height;
+}
+
+Canvas::~Canvas()
+{
+	closegraph();									// 关闭窗口
+}
+
+void Canvas::setPixel(int x, int y, COLORREF color)
+{
+	if (x > 0 && x < mScrWidth && y > 0 && y < mScrHeight)
+	{
+		y = mScrHeight - y;							// 翻转y轴
+		pImageBuf[x + y * mScrWidth] = BGR(color);
+	}
+}
